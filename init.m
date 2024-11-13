e782c5d1f18cda06b10455052f5ae29e7390e430
Alex Gisi <apgisi11@gmail.com>
2024-11-13 15:32:11 -0600

    Add readme

--- init.m
+++ init.m
@@ -7,12 +7,12 @@ fr = 0.005;
 a = 0.8;
 b = 0.8;
 mu = 0.9;
-d0 = 0.5;  % must have d0 < a to prevent skidding
+d0 = 0.001;  % must have d0 < a to prevent skidding
 
 M = diag([m; m; I]);
 M_inv = inv(M);
 
-state0 = [0; 0; 0.2];
+state0 = [0; 0; 0.0];
 
 tau_max = 3.88*2;  % computed from big-step.csv warthog data and using 0.4635 as the wheel moment; doubled because there's two motors
 tau_min = -tau_max;  
---
+++ readme.md
@@ -0,0 +1,7 @@
+# caracciolo-1998-control-implementation
+An implementation of the four-wheel differential drive vehicle model and open-loop nonlinear feedback law derived in [Caracciolo 1999](https://doi.org/10.1109/ROBOT.1999.773994).
+
+## Example
+Vehicle body longitudinal velocity tracking a sinusoidal reference input:
+
+![](img/sine-input.png)
